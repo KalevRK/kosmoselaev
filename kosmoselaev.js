8349a672c4aff8c35301050acfc17a3324dd7782
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-02-18 19:22:45 -0800

    Change player movement from keyboard-based to mouse-based.

--- kosmoselaev.js
+++ kosmoselaev.js
@@ -9,9 +9,11 @@ var settings = {
 var playerSettings = {
     x: settings.width/2,
     y: settings.height-20,
-    width: 10,
-    height: 10,
-    moveIncrement: 10
+    width: 20,
+    height: 20,
+    moveIncrement: 20,
+    laserWidth: 5,
+    laserHeight: 10
 };
 
 // Helper function for creating screen coordinates
@@ -40,19 +42,29 @@ var player = d3.select('#gameScreen').append('svg:rect').style({
 .attr('x', playerSettings.x)
 .attr('y', playerSettings.y);
 
+// move player relative to mouse
+d3.select('#game').on('mousemove', function() {
+  var location = d3.mouse(this);
+  playerSettings.x = location[0];
+  playerSettings.y = location[1];
+  player.attr('x', playerSettings.x)
+  .attr('y', playerSettings.y);
+})
+
 // handle keyboard events for the player
 d3.select('body').on('keydown', function(d) {
     d3.event.stopPropagation();
     var key = d3.event.keyCode;
-    if (key === 39) {
-      // move right
-      playerSettings.x += playerSettings.moveIncrement;
-      player.attr('x', playerSettings.x);
-    } else if (key === 37) {
-        // move left
-        playerSettings.x -= playerSettings.moveIncrement;
-        player.attr('x', playerSettings.x);
-    }
+    if (key === 32) {
+        // fire weapon
+        d3.select('#gameScreen').append('svg:rect').style({
+            fill: 'green'
+        })
+        .attr('width', addPx(playerSettings.laserWidth))
+        .attr('height', addPx(playerSettings.laserHeight))
+        .attr('x', playerSettings.x + playerSettings.width/2)
+        .attr('y', playerSettings.y - playerSettings.height/2);
+    } 
 
 })
 
@@ -61,6 +73,7 @@ d3.select('body').on('keydown', function(d) {
 
 // update game based on events
 function update() {
+    d3.select('.laser')
 };
 
 // Loop checking for game events

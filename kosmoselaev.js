dfa738ab444eb2c7cdd66e7878661a11f6be8448
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2015-02-18 22:15:18 -0800

    In progress implementation of laser clean up.

--- kosmoselaev.js
+++ kosmoselaev.js
@@ -16,6 +16,11 @@ var playerSettings = {
     laserHeight: 10
 };
 
+// Lasers
+var lasers = {
+  playerLasers: []
+};
+
 // Helper function for creating screen coordinates
 var addPx = function(number) {
   return number + 'px';
@@ -34,19 +39,20 @@ d3.select('#gameScreen').style({
 });
 
 // initialize the player
-var player = d3.select('#gameScreen').append('svg:rect').style({
-  fill: 'black'
-})
+var player = d3.select('#gameScreen').append('svg:rect')
 .attr('width', addPx(playerSettings.width))
 .attr('height', addPx(playerSettings.height))
 .attr('x', playerSettings.x)
-.attr('y', playerSettings.y);
+.attr('y', playerSettings.y)
+.style({
+  fill: 'black'
+});
 
 // move player relative to mouse
 d3.select('#game').on('mousemove', function() {
   var location = d3.mouse(this);
-  playerSettings.x = location[0];
-  playerSettings.y = location[1];
+  playerSettings.x = Math.min(Math.max(0,location[0]),settings.width-playerSettings.width);
+  playerSettings.y = Math.min(Math.max(0,location[1]),settings.height-playerSettings.height);
   player.attr('x', playerSettings.x)
   .attr('y', playerSettings.y);
 })
@@ -63,12 +69,21 @@ d3.select('body').on('keydown', function(d) {
 })
 
 var fireWeapon = function() {
-  d3.select('#gameScreen').append('svg:rect')
+  // push new laser shot to array
+  lasers.playerLasers.push(1);
+
+  // bind laser shot array data to .laser class elements
+  // animate across screen
+  d3.select('#gameScreen').selectAll('.laser')
+    .data(lasers.playerLasers).enter().append('svg:rect')
+    .classed('laser', true)
     .style({ fill: 'green' })
     .attr('width', addPx(playerSettings.laserWidth))
     .attr('height', addPx(playerSettings.laserHeight))
     .attr('x', playerSettings.x + playerSettings.width/2)
-    .attr('y', playerSettings.y - playerSettings.height/2);
+    .attr('y', playerSettings.y - playerSettings.height/2)
+    .transition().ease('linear').duration(1000)
+    .attr('y', 0);
 };
 
 // Loop enemy motion
@@ -76,7 +91,15 @@ var fireWeapon = function() {
 
 // update game based on events
 function update() {
-    d3.select('.laser')
+
+  // check laser positions and clean up lasers that reach end of screen
+  d3.selectAll('.laser').each( function() {
+    var laser = d3.select(this);
+    if (laser.attr('y') < 1) {
+      lasers.playerLasers.shift();
+      d3.selectAll('.laser').data(lasers.playerLasers).exit().remove();
+    }
+  });
 };
 
 // Loop checking for game events
